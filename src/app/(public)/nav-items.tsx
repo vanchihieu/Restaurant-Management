7cad0cfe8d8eb263a7b398baf81f778e07444a4f
Van Chi Hieu <vanchihieu3@gmail.com>
2024-07-23 14:05:41 +0700

    ưu nhược 2 cơ chế quản lý đăng nhập ở server và client

--- src/app/(public)/nav-items.tsx
+++ src/app/(public)/nav-items.tsx
@@ -1,34 +1,47 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
+import { getAccessTokenFromLocalStorage } from "@/lib/utils";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const menuItems = [
   {
-    title: 'Món ăn',
-    href: '/menu'
+    title: "Món ăn",
+    href: "/menu",
   },
   {
-    title: 'Đơn hàng',
-    href: '/orders'
+    title: "Đơn hàng",
+    href: "/orders",
   },
   {
-    title: 'Đăng nhập',
-    href: '/login',
-    authRequired: false
+    title: "Đăng nhập",
+    href: "/login",
+    authRequired: false, // chưa đăng nhập sẽ hiển thị
   },
   {
-    title: 'Quản lý',
-    href: '/manage/dashboard',
-    authRequired: true
-  }
-]
+    title: "Quản lý",
+    href: "/manage/dashboard",
+    authRequired: true, // đã đăng nhập mới hiển thị
+  },
+];
 
 export default function NavItems({ className }: { className?: string }) {
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    setIsAuth(Boolean(getAccessTokenFromLocalStorage()));
+  }, []);
+
   return menuItems.map((item) => {
+    if (
+      (item.authRequired === true && !isAuth) ||
+      (item.authRequired === false && isAuth)
+    )
+      return null;
     return (
       <Link href={item.href} key={item.href} className={className}>
         {item.title}
       </Link>
-    )
-  })
+    );
+  });
 }
