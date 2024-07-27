005e045ba70249b794a1e8f3ee96f614252d21c3
Van Chi Hieu <vanchihieu3@gmail.com>
2024-07-27 22:33:25 +0700

    Fix bug menu không thay đổi khi đăng nhập hoặc logout thành công

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "localhost",
+        pathname: "/**",
+      },
+      {
+        hostname: "via.placeholder.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -30,10 +30,13 @@ export default function Home() {
             .map((_, index) => (
               <div className="flex gap-4 w" key={index}>
                 <div className="flex-shrink-0">
-                  <img
-                    src="https://ik.imagekit.io/freeflo/production/6b91c700-92c4-4601-8e96-37d84ac3c28c.png?tr=w-2048,q-75&alt=media&pr-true"
+                  <Image
+                    src="/banner.png"
+                    width={400}
+                    height={200}
+                    quality={100}
+                    alt="Banner"
                     className="object-cover w-[150px] h-[150px] rounded-md"
-                    alt="Bánh mì"
                   />
                 </div>
                 <div className="space-y-1">
