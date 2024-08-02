500b23d64edafbaca19326df61a3fc94fe0ff45a
Van Chi Hieu <vanchihieu3@gmail.com>
2024-08-02 15:44:34 +0700

    Fetch danh sách món ăn ở trang chủ

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,9 +1,24 @@
+import dishApiRequest from "@/apiRequests/dish";
+import { formatCurrency } from "@/lib/utils";
+import { DishListResType } from "@/schemaValidations/dish.schema";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  let dishList: DishListResType["data"] = [];
+  try {
+    const result = await dishApiRequest.list();
+    const {
+      payload: { data },
+    } = result;
+    dishList = data;
+  } catch (error) {
+    return <div>Something went wrong</div>;
+  }
+
   return (
     <div className="w-full space-y-4">
-      <div className="relative">
+      <section className="relative">
         <span className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-10"></span>
         <Image
           src="/banner.png"
@@ -21,31 +36,33 @@ export default function Home() {
             Vị ngon, trọn khoảnh khắc
           </p>
         </div>
-      </div>
+      </section>
       <section className="space-y-10 py-16">
         <h2 className="text-center text-2xl font-bold">Đa dạng các món ăn</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
-          {Array(4)
-            .fill(0)
-            .map((_, index) => (
-              <div className="flex gap-4 w" key={index}>
-                <div className="flex-shrink-0">
-                  <Image
-                    src="/banner.png"
-                    width={400}
-                    height={200}
-                    quality={100}
-                    alt="Banner"
-                    className="object-cover w-[150px] h-[150px] rounded-md"
-                  />
-                </div>
-                <div className="space-y-1">
-                  <h3 className="text-xl font-semibold">Bánh mì</h3>
-                  <p className="">Bánh mì sandwidch</p>
-                  <p className="font-semibold">123,123đ</p>
-                </div>
+          {dishList.map((dish) => (
+            <Link
+              href={`/dishes/${dish.id}`}
+              className="flex gap-4 w"
+              key={dish.id}
+            >
+              <div className="flex-shrink-0">
+                <Image
+                  src={dish.image}
+                  width={150}
+                  height={150}
+                  quality={100}
+                  alt={dish.name}
+                  className="object-cover w-[150px] h-[150px] rounded-md"
+                />
               </div>
-            ))}
+              <div className="space-y-1">
+                <h3 className="text-xl font-semibold">{dish.name}</h3>
+                <p className="">{dish.description}</p>
+                <p className="font-semibold">{formatCurrency(dish.price)}</p>
+              </div>
+            </Link>
+          ))}
         </div>
       </section>
     </div>
