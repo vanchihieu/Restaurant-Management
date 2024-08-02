5f119f04b16f40e27c6a199a6e29802e9591541d
Van Chi Hieu <vanchihieu3@gmail.com>
2024-08-02 10:09:21 +0700

    Thêm bàn ăn

--- src/app/manage/tables/add-table.tsx
+++ src/app/manage/tables/add-table.tsx
@@ -1,52 +1,127 @@
-'use client'
-import { Button } from '@/components/ui/button'
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { PlusCircle } from 'lucide-react'
-import { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
-import { getVietnameseTableStatus } from '@/lib/utils'
-import { CreateTableBody, CreateTableBodyType } from '@/schemaValidations/table.schema'
-import { TableStatus, TableStatusValues } from '@/constants/type'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { PlusCircle } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { getVietnameseTableStatus, handleErrorApi } from "@/lib/utils";
+import {
+  CreateTableBody,
+  CreateTableBodyType,
+} from "@/schemaValidations/table.schema";
+import { TableStatus, TableStatusValues } from "@/constants/type";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useAddTableMutation } from "@/queries/useTable";
+import { toast } from "@/components/ui/use-toast";
 
 export default function AddTable() {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
+
+  const addTableMutation = useAddTableMutation();
+
   const form = useForm<CreateTableBodyType>({
     resolver: zodResolver(CreateTableBody),
     defaultValues: {
       number: 0,
       capacity: 2,
-      status: TableStatus.Hidden
+      status: TableStatus.Hidden,
+    },
+  });
+
+  const reset = () => {
+    form.reset();
+  };
+
+  const onSubmit = async (values: CreateTableBodyType) => {
+    if (addTableMutation.isPending) return;
+    try {
+      const result = await addTableMutation.mutateAsync(values);
+      toast({
+        description: result.payload.message,
+      });
+      reset();
+      setOpen(false);
+    } catch (error) {
+      handleErrorApi({
+        error,
+        setError: form.setError,
+      });
     }
-  })
+  };
+
   return (
-    <Dialog onOpenChange={setOpen} open={open}>
+    <Dialog
+      onOpenChange={(value) => {
+        if (!value) {
+          reset();
+        }
+        setOpen(value);
+      }}
+      open={open}
+    >
       <DialogTrigger asChild>
-        <Button size='sm' className='h-7 gap-1'>
-          <PlusCircle className='h-3.5 w-3.5' />
-          <span className='sr-only sm:not-sr-only sm:whitespace-nowrap'>Thêm bàn</span>
+        <Button size="sm" className="h-7 gap-1">
+          <PlusCircle className="h-3.5 w-3.5" />
+          <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+            Thêm bàn
+          </span>
         </Button>
       </DialogTrigger>
-      <DialogContent className='sm:max-w-[600px] max-h-screen overflow-auto' onCloseAutoFocus={() => form.reset()}>
+      <DialogContent
+        className="sm:max-w-[600px] max-h-screen overflow-auto"
+        onCloseAutoFocus={() => form.reset()}
+      >
         <DialogHeader>
           <DialogTitle>Thêm bàn</DialogTitle>
         </DialogHeader>
         <Form {...form}>
-          <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8' id='add-table-form'>
-            <div className='grid gap-4 py-4'>
+          <form
+            noValidate
+            className="grid auto-rows-max items-start gap-4 md:gap-8"
+            id="add-table-form"
+            onSubmit={form.handleSubmit(onSubmit, (e) => {
+              console.log(e);
+            })}
+            onReset={reset}
+          >
+            <div className="grid gap-4 py-4">
               <FormField
                 control={form.control}
-                name='number'
+                name="number"
                 render={({ field }) => (
                   <FormItem>
-                    <div className='grid grid-cols-4 items-center justify-items-start gap-4'>
-                      <Label htmlFor='name'>Số hiệu bàn</Label>
-                      <div className='col-span-3 w-full space-y-2'>
-                        <Input id='number' type='number' className='w-full' {...field} />
+                    <div className="grid grid-cols-4 items-center justify-items-start gap-4">
+                      <Label htmlFor="name">Số hiệu bàn</Label>
+                      <div className="col-span-3 w-full space-y-2">
+                        <Input
+                          id="number"
+                          type="number"
+                          className="w-full"
+                          {...field}
+                        />
                         <FormMessage />
                       </div>
                     </div>
@@ -55,13 +130,18 @@ export default function AddTable() {
               />
               <FormField
                 control={form.control}
-                name='capacity'
+                name="capacity"
                 render={({ field }) => (
                   <FormItem>
-                    <div className='grid grid-cols-4 items-center justify-items-start gap-4'>
-                      <Label htmlFor='price'>Lượng khách cho phép</Label>
-                      <div className='col-span-3 w-full space-y-2'>
-                        <Input id='capacity' className='w-full' {...field} type='number' />
+                    <div className="grid grid-cols-4 items-center justify-items-start gap-4">
+                      <Label htmlFor="price">Lượng khách cho phép</Label>
+                      <div className="col-span-3 w-full space-y-2">
+                        <Input
+                          id="capacity"
+                          className="w-full"
+                          {...field}
+                          type="number"
+                        />
                         <FormMessage />
                       </div>
                     </div>
@@ -70,16 +150,19 @@ export default function AddTable() {
               />
               <FormField
                 control={form.control}
-                name='status'
+                name="status"
                 render={({ field }) => (
                   <FormItem>
-                    <div className='grid grid-cols-4 items-center justify-items-start gap-4'>
-                      <Label htmlFor='description'>Trạng thái</Label>
-                      <div className='col-span-3 w-full space-y-2'>
-                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                    <div className="grid grid-cols-4 items-center justify-items-start gap-4">
+                      <Label htmlFor="description">Trạng thái</Label>
+                      <div className="col-span-3 w-full space-y-2">
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}
+                        >
                           <FormControl>
                             <SelectTrigger>
-                              <SelectValue placeholder='Chọn trạng thái' />
+                              <SelectValue placeholder="Chọn trạng thái" />
                             </SelectTrigger>
                           </FormControl>
                           <SelectContent>
@@ -101,11 +184,11 @@ export default function AddTable() {
           </form>
         </Form>
         <DialogFooter>
-          <Button type='submit' form='add-table-form'>
+          <Button type="submit" form="add-table-form">
             Thêm
           </Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
